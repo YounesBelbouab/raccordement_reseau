a13e40ce082a8e1f1b68a577b3b96da1808b4246
Belbouab <Belbouab.Younes@gmail.com>
2024-10-17 13:12:35 +0200

    ajout de tous les fichiers utile au projet

---
+++ sandbox/sandbox.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+reseauArbre = pd.read_csv("reseau_en_arbre.csv")
+
+a_remplacer = reseauArbre[reseauArbre["infra_type"] == "a_remplacer"]
+intact = reseauArbre[reseauArbre["infra_type"] == "infra_intacte"]
+
+pd.set_option('display.max_rows', 1000)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
+
+a_remplacer.drop_duplicates(['id_batiment'], inplace=True)
+print(a_remplacer.groupby("infra_id").value_counts().head(588))
+
+nouveau_reseau_arbre = pd.merge(left=reseauArbre, right=a_remplacer, on="infra_id", suffixes=('', '_y'))
+colonnes_a_garder = [col for col in nouveau_reseau_arbre.columns if not col.endswith('_y')]
+
+reseau_arbre_final = nouveau_reseau_arbre[colonnes_a_garder]
+reseau_arbre_final["longueur_par_maison"] = reseau_arbre_final["longueur"]/reseau_arbre_final["nb_maisons"]
+
+reseau_trie = reseau_arbre_final.sort_values(by="longueur_par_maison", ascending=False)
+print(reseau_trie.head(20))
