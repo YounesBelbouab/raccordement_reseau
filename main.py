938f73fb842a9a3a27eed6f820e3bbb81bcd6036
Mehdi Ben Cheikh <bencheikh.mehdi.mb@gmail.com>
2024-10-18 14:46:03 +0200

    Groupeby et calcul du total des difficultés pour chaque batiment

--- main.py
+++ main.py
@@ -14,20 +14,32 @@ total_maisons_par_infra = a_remplacer_cleaned.groupby('infra_id')['nb_maisons'].
 a_remplacer_cleaned_merged = pd.merge(a_remplacer_cleaned, total_maisons_par_infra, on='infra_id', suffixes=('', '_total'))
 
 # Recalculer la longueur par maison en divisant par le nombre total de maisons pour chaque 'infra_id'
-a_remplacer_cleaned_merged['longueur_par_maison'] = a_remplacer_cleaned_merged['longueur'] / a_remplacer_cleaned_merged['nb_maisons_total']
+a_remplacer_cleaned_merged['difficulté'] = a_remplacer_cleaned_merged['longueur'] / a_remplacer_cleaned_merged['nb_maisons_total']
 
 a_remplacer_cleaned_merged['priorite'] = pd.cut(
-    a_remplacer_cleaned_merged['longueur_par_maison'], 
+    a_remplacer_cleaned_merged['difficulté'], 
     bins=[float(0.195617), float(0.334317), float(1.219881), float(4.928491), float(42.114422)+1],  # Définir les seuils de longueur_par_maison
     labels=[1, 2, 3, 4]  # Attribuer des priorités : 1 (petites valeurs) à 4 (grandes valeurs)
 )
 # Trier les résultats en fonction de la nouvelle colonne 'longueur_par_maison'
-reseau_trie_final = a_remplacer_cleaned_merged.sort_values(by='longueur_par_maison', ascending=True)
+reseau_trie_final = a_remplacer_cleaned_merged.sort_values(by='difficulté', ascending=True)
 
-reseau_trie_final = a_remplacer_cleaned_merged.sort_values(by=['priorite', 'longueur_par_maison'], ascending=[True, True])
+reseau_trie_final = a_remplacer_cleaned_merged.sort_values(by=['priorite', 'difficulté'], ascending=[True, True])
+
+reseauArbreV2 = reseau_trie_final.drop_duplicates(subset=["infra_id"])
+
+
+# Regrouper les données par 'id_batiment' et calculer le total des difficultés pour chaque batiment
+total_difficulté_par_batiment = reseau_trie_final.groupby('id_batiment')['difficulté'].sum().reset_index()
+
+# Fusionner ces informations avec le DataFrame des infrastructures à remplacer
+total_difficulté_par_batiment_merged = pd.merge(reseau_trie_final, total_difficulté_par_batiment, on='id_batiment', suffixes=('', '_total'))
+
+# Afficher le résultat
+print(total_difficulté_par_batiment_merged)
 
 
 # Afficher les 20 premières lignes pour vérifier
-print(reseau_trie_final.head(537))
+# print(reseau_trie_final.head(537))
 
 # print(reseau_trie_final.describe())
