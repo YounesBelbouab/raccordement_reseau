c2ce6c40db2eda620fd8a247bee7214aa4c6c932
YounesBelbouab <155763906+YounesBelbouab@users.noreply.github.com>
2024-11-05 17:06:51 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,27 +3,30 @@ from Infrastructure import Infra
 import pandas as pd
 
 df = pd.read_csv("final_infra.csv")
-#print(df.head())
+df = df[df["infra_type"] == "a_remplacer"]
 
 dict_infra = {}
 
 for i in range(0,len(df)):
-    dict_infra[df["infra_id"][i]] = Infra(df["infra_id"][i], df["longueur"][i], df["infra_type"][i], df["nb_maisons_total"][i])
+    dict_infra[df["infra_id"][i]] = Infra(df["infra_id"][i], df["longueur"][i], df["infra_type"][i], df["nb_maisons"][i])
 
 liste_batiment = []
 batiment_fini = []
 infra_fini = []
 
 
-for i, y in df.groupby(by="id_batiment"):
-    liste_infra = [dict_infra[infra_id] for infra_id in y["infra_id"].values]
-    liste_batiment.append(Batiment(i, liste_infra))
+for batiment_id in df["id_batiment"].unique():
+    infra_id_df = df[df["id_batiment"] == batiment_id]["infra_id"]
+    liste_infra = [dict_infra[infra_id] for infra_id in infra_id_df if infra_id in dict_infra]
+    batiment = Batiment(batiment_id, liste_infra)
+    liste_batiment.append(batiment)
 
-liste_batiment.sort(key=lambda bat: bat.get_batiment_difficulty())
+
+liste_batiment.sort(key=lambda batiment: batiment.get_batiment_difficulty())
 
 
 while liste_batiment:
-    liste_batiment.sort(key=lambda bat: bat.get_batiment_difficulty())
+    liste_batiment.sort(key=lambda batiment: batiment.get_batiment_difficulty())
     batiment = liste_batiment.pop(0)
     
     for infra in batiment.liste_infra:
@@ -33,3 +36,5 @@ while liste_batiment:
     batiment_fini.append(batiment)
 
 
+print(liste_batiment)
+print([batiment.id_bat for batiment in batiment_fini])
